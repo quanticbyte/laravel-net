d9e0425a06d9b538c040ed9ff64286a279e68c9c
quanticbyte <paucatalan80@gmail.com>
2018-07-10 16:11:44 +0200

    creado las migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->integer('rol')->unsigned();
+            $table->boolean('is_active')->default(1);
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
